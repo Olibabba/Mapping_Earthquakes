d8837ced2e86ee4670869a1a12d886776b26eda3
olibabba <oliver.dykstra@gmail.com>
2022-06-13 23:39:31 -0500

    new folder stuff

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,28 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+var map = L.map('mapid').setView([34.0522, -118.2437], 16);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// }).addTo(map);
+
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1',
+    fillOpacity: 0.5,
+    radius: 300
+ }).addTo(map);
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
