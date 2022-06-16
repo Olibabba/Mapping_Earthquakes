9e127feb42cf14e05dded2220a2951c7f0385b21
olibabba <oliver.dykstra@gmail.com>
2022-06-15 22:04:50 -0500

    much done

--- Mapping_GEOJSON_Points/static/js/logic.js
+++ Mapping_GEOJSON_Points/static/js/logic.js
@@ -10,7 +10,7 @@ const cityData = cities
 
 
 
-var map = L.map('mapid').setView([38.0522, -99.2437], 4);
+var map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 19,
@@ -24,6 +24,14 @@ var map = L.map('mapid').setView([38.0522, -99.2437], 4);
 //     fillOpacity: 0.5,
 //     radius: 300
 //  }).addTo(map);
+
+L.geoJSON(sanFranAirport, {
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+    }
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -47,6 +55,8 @@ cityData.forEach(function(city) {
     .addTo(map);
 });
 
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
