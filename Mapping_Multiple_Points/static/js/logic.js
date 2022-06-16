5f1e66b20ec535e10f67166d06e2602b8e3b76e5
olibabba <oliver.dykstra@gmail.com>
2022-06-15 21:16:15 -0500

    multiple points plotted

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,20 +1,29 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+console.log(cities)
 
-var map = L.map('mapid').setView([34.0522, -118.2437], 16);
+const cityData = cities
+// Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     L.marker(city.location).addTo(map);
+// });
+
+
+
+var map = L.map('mapid').setView([38.0522, -99.2437], 4);
 
 // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 19,
 //     attribution: '© OpenStreetMap'
 // }).addTo(map);
 
-var marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circle([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: '#ffffa1',
-    fillOpacity: 0.5,
-    radius: 300
- }).addTo(map);
+// var marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#ffffa1',
+//     fillOpacity: 0.5,
+//     radius: 300
+//  }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -22,6 +31,21 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-ni
     accessToken: API_KEY
 });
 
+// for (let i = 0; i < cityData.length; i++){
+//     console.log(cityData[i].location)
+//     L.marker(cityData[i].location).addTo(map);
+// }
+cityData.forEach(function(city) {
+    console.log(city.location)
+    L.circle(city.location, {
+        color: 'orange',
+        fillColor: 'orange',
+        fillOpacity: 0.75,
+        radius: city.population/40
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 
 // Then we add our 'graymap' tile layer to the map.
