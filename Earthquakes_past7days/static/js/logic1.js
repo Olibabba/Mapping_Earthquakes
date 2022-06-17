ad22b8b8dd433226a816d591fe8df4b77687e9cb
olibabba <oliver.dykstra@gmail.com>
2022-06-16 21:50:21 -0500

    mapped dem earthquakes

--- Earthquakes_past7days/static/js/logic1.js
+++ Earthquakes_past7days/static/js/logic1.js
@@ -8,7 +8,11 @@ const cityData = cities
 //     L.marker(city.location).addTo(map);
 // });
 
-
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
 
 // var map = L.map('mapid').setView([30, 30], 2);
 
@@ -39,7 +43,7 @@ let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles
     accessToken: API_KEY
 });
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let navStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -50,28 +54,35 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+let satStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 // Create a base layer that holds both maps.
 let baseMaps = {
     Light: light,
-    Street: streets,
+    Street: navStreets,
+    Satellite: satStreets,
     Dark: dark
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [light]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [satStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/Olibabba/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GEOJSON_Points/static/js/majorAirports.json";
-let torontoData = "https://raw.githubusercontent.com/Olibabba/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
+// let airportData = "https://raw.githubusercontent.com/Olibabba/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GEOJSON_Points/static/js/majorAirports.json";
+// let torontoData = "https://raw.githubusercontent.com/Olibabba/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
 
 // for (let i = 0; i < cityData.length; i++){
 //     console.log(cityData[i].location)
@@ -90,10 +101,10 @@ let torontoData = "https://raw.githubusercontent.com/Olibabba/Mapping_Earthquake
 // });
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-    console.log(data);
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
-});
+//   L.geoJSON(data).addTo(map);
+// });
     
     
