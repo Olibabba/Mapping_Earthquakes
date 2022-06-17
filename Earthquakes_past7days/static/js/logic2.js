6b208e6629277e511e76a7425339e494b9b67bf9
olibabba <oliver.dykstra@gmail.com>
2022-06-16 22:15:59 -0500

    color and popup

--- Earthquakes_past7days/static/js/logic2.js
+++ Earthquakes_past7days/static/js/logic2.js
@@ -18,7 +18,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
@@ -33,9 +33,32 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
+  function getColor(magnitude) {
+    if (magnitude >= 0 && magnitude <= 1) {
+      return "#e9d8a6";
+    }
+    if (magnitude > 1 && magnitude <= 2) {
+      return "#ffb703";
+    }
+    if (magnitude > 2 && magnitude <= 3) {
+      return "#fb8500";
+    }
+    if (magnitude > 3 && magnitude <= 4) {
+      return "#8ecae6";
+    }
+    if (magnitude > 4 && magnitude <= 5) {
+      return "#4361ee";
+    }
+    if (magnitude > 5) {
+      return "#ffd60a";
+    }
+  }
   L.geoJSON(data, {
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng)
+    },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup((`<h3>Magnitude: ${feature.properties.mag}</h3><hr><h4>Location: ${feature.properties.place}</h4>`)); 
     },
     style: styleInfo
   }).addTo(map);
